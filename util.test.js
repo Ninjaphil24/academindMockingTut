002539416bf1f9c2a55526b5f07da2fe7f1b587f
Ninjaphil24 <mphilippos@hotmail.com>
2022-04-30 17:55:41 +0300

    Test with API works

---
+++ util.test.js
@@ -0,0 +1,8 @@
+const { loadTitle }=require('./util')
+
+test('should print an uppercase text',()=>{
+    // expect(printTitle()).toBe('DELECTUS AUT AUTEM')
+    loadTitle().then(title=>{
+        expect(title).toBe('DELECTUS AUT AUTEM')
+    })
+})
